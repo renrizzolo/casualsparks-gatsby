da0a867e748b53e26e8fc0690f97a5adc6a976c5
Ren Rizzolo <renrizmusic@gmail.com>
2019-01-22 15:56:15 +1100

    circle layout own component

---
+++ src/components/CircleLayout/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Zoom, Fade, FadeZoom } from '../../animations'
+
+const items = [
+  {
+    key: 'cs-circle-1',
+    className: 'cs-circle cs-circle__outer-1',
+  },
+  {
+    key: 'cs-circle-2',
+    className: 'cs-circle cs-circle__outer-2',
+  },
+  {
+    key: 'cs-circle-3',
+    className: 'cs-circle cs-circle__outer-3',
+  }
+]
+
+const CircleLayout = ({ children, show }) => (
+  <Fade show={true}>
+    {style => 
+    <div style={style} className="flex-center flex-container__row viewport-half">
+      <Zoom
+        show={show}
+        className="cs-circle darker" id="cs-circle"
+        style={{ backgroundImage: 'url(\'https://unsplash.it/700/?random\')', backgroundSize: 'cover', backgroundBlendMode: 'overlay' }}
+      />
+      {children}
+      <FadeZoom items={items} />
+    </div>
+    }
+  </Fade>
+)
+export default CircleLayout;
