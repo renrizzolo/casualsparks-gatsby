a42888a3492c91137cc15a9b62873edbda204a19
Ren Rizzolo <renrizmusic@gmail.com>
2019-01-22 15:55:47 +1100

    use page context to display the correct layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from "gatsby"
-
+import CircleLayout from './CircleLayout';
+import { Fade } from '../animations';
 import Navbar from '../components/Navbar'
-import './all.sass'
+import '../styles/index.scss'
 
-const TemplateWrapper = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+class TemplateWrapper extends Component {
+  
+  
+  render() {
+
+    const { children, pageContext } = this.props;
+    console.log('context', pageContext);
+    return (
+      <StaticQuery
+        query={graphql`
       query HeadingQuery {
           site {
             siteMetadata {
@@ -17,30 +25,40 @@ const TemplateWrapper = ({ children }) => (
           }
         }
     `}
-    render={data => (
-      <div>
-        <Helmet>
-          <html lang="en" />
-          <title>{data.site.siteMetadata.title}</title>
-          <meta name="description" content={data.site.siteMetadata.description} />
-          
-          <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
-	        <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-	        <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-	
-	        <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
-	        <meta name="theme-color" content="#fff" />
+        render={data => (
+          <div>
+            <Helmet>
+              <html lang="en" />
+              <title>{data.site.siteMetadata.title}</title>
+              <meta name="description" content={data.site.siteMetadata.description} />
 
-	        <meta property="og:type" content="business.business" />
-          <meta property="og:title" content={data.site.siteMetadata.title} />
-          <meta property="og:url" content="/" />
-          <meta property="og:image" content="/img/og-image.jpg" />
-        </Helmet>
-        <Navbar />
-        <div>{children}</div>
-      </div>
-    )}
-  />
-)
+              <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+              <link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
+              <link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
+
+              <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#ff4400" />
+              <meta name="theme-color" content="#fff" />
+
+              <meta property="og:type" content="business.business" />
+              <meta property="og:title" content={data.site.siteMetadata.title} />
+              <meta property="og:url" content="/" />
+              <meta property="og:image" content="/img/og-image.jpg" />
+            </Helmet>
+            <Navbar />
+              {pageContext && pageContext.layout === 'circle' ?
+                <CircleLayout show={!this.state.mounted}>
+                  {children}
+                </CircleLayout>
+                :
+                <Fade show={this.state.mounted}>
+                  {style => <div style={style}>{children}</div>}
+                </Fade>
+              }
+          </div>
+        )}
+      />
+    )
+  }
+}
 
 export default TemplateWrapper
