0b6aa5952af5de263409b4af265d3392cb65f69f
renrizzolo <renrizmusic@gmail.com>
2019-01-22 23:53:25 +1100

    use Releases component

--- src/templates/music-page.js
+++ src/templates/music-page.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql } from 'gatsby'
+import { StaticQuery, graphql } from 'gatsby'
 import { Fade } from '../animations';
 import { Link } from '@reach/router';
 import '../styles/common/item-grid.scss';
-import ToggleClass from '../components/ToggleClass';
-import WatchConnection from '../components/WatchConnection';
-import OfflineError from '../components/OfflineError';
+import ReleaseItem from '../components/ReleaseItem';
 import PageLayout from '../components/PageLayout';
-
+import Releases from '../components/Releases';
 const MusicPageTemplate = ({title, sections}) => {
 	return (
 		<PageLayout title={title}  backgroundColor="blue">
@@ -22,70 +20,15 @@ const Grid = ({sections}) => {
 				sections.map(section => (
 					<section className="item-grid">
 						<h1 className="heading item-grid__heading"><span className="dark-blue-bkg">{section.title}</span></h1>
-						{	section.items.map((item) => (
-							<Item
-								key={item.id}
-								data={item}
-							/>
-							))
-							}
-						</section>
-					))
+						<Releases section={section}/>
+				</section>
+
+				))
 				
 		);
 }
 
 
-const Item = (props) => {
-	const { data } = props;
-	return (
-			<div className="item background-pearl">
-				<header>
-					<div className="item-info">
-						<h1 className="item-heading">{data.artist}</h1>
-					{data.id ? <Link to={`/release/${data.id}`}><h2 className="item-sub-heading">{data.name}</h2></Link>
-						:
-						<h2 className="item-sub-heading">{data.name}</h2>
-					}
-						<div className="link-container">
-							{data.links && data.links.map((link) => (
-								<a key={link.name} target="_blank" rel="noopener" className="button light-blue shop-link" href={link.url}>{link.name}</a>
-							))
-							}
-						</div>
-					</div>
-				</header>
-
-				<WatchConnection render={ online => (
-					online ? 
-					(data.previewHTML &&
-						<div dangerouslySetInnerHTML={{ __html: data.previewHTML }} />
-					)
-					:
-						<OfflineError/>
-					)}
-				/>
-
-				{data.trackList &&
-					<aside>
-						<ToggleClass className="track-list" toggleClass="expanded">
-							{toggle => (
-								<div>
-								<h2 onMouseUp={toggle}>Track list <span></span></h2>
-									<ol>
-									{	data.trackList.map((track, i) => (
-										<li key={i}>{track}</li>
-										))
-									}
-									</ol>
-								</div>
-							)}
-						</ToggleClass>
-					</aside>
-				}
-			</div>
-	)
-}
 
 // MusicPageTemplate.propTypes = {
 // 	image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -138,19 +81,8 @@ export const musicPageQuery = graphql`
       frontmatter {
         title
         sections {
-          title 
-          items {
-						name
-						artist
-						id
-						previewHTML
-		    		description
-						trackList
-						links {
-							name
-							url
-						}
-					}
+					title
+					type
         }  
       }
     }
