4e9c140eaa2db391a1dad425acbe8b4fceee6308
Ren Rizzolo <renrizmusic@gmail.com>
2019-01-23 16:18:12 +1100

    add weird toggle so it all fits in the stupid circle

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -6,23 +6,36 @@ import SocialLink from '../components/SocialLink'
 import CircleLayout from '../components/CircleLayout';
 import { Fade } from '../animations/';
 import ContactForm from '../components/ContactForm';
+import ToggleClass from '../components/ToggleClass';
+import PageLayout from '../components/PageLayout';
 
 export const ContactPageTemplate = ({ title, links }) => {
-
   return (
-        <Fade>
-        {style => 
-          <div style={style} className="circle-text flex-container__column">
-            <h1 className="heading">{title}</h1>
-              <div className="social">
-                {links.map((item, i) => (
-                  <SocialLink key={i} title={item.label} url={item.url} icon={item.icon} />
-                ))}
-              </div>
-              <ContactForm/>			  
-          </div>
-        }
-      </Fade>
+    <Fade>
+      {style => 
+        <div style={style} className="circle-text flex-container__column">
+            <ToggleClass>
+              {(toggle, toggled) => 
+                <div>
+                  {!toggled ?
+                    <div>
+                      <h1 className="heading">{title}</h1>
+                      <div className="social">
+                        {links.map((item, i) => (
+                          <SocialLink key={i} title={item.label} url={item.url} icon={item.icon} />
+                        ))}
+                      </div>
+                    </div>
+                    :
+                    <ContactForm/>	
+                  }
+                  <button class="button dark-blue" onClick={toggle}>{!toggled ? 'email →' : '←'}</button>
+                </div>		  
+              }
+             </ToggleClass>
+        </div>
+      }
+    </Fade>
   )
 }
 
