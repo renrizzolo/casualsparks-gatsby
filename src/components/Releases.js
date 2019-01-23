f26f0fa3c69346d4f578ac51adc9342cb07a3efb
Ren Rizzolo <renrizmusic@gmail.com>
2019-01-23 14:38:06 +1100

    use Trail on releases

--- src/components/Releases.js
+++ src/components/Releases.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 import ReleaseItem from './ReleaseItem'
+import { Trail } from 'react-spring'
 
 const Releases = ({section}) => {
   console.log(section);
@@ -28,19 +29,22 @@ const Releases = ({section}) => {
 								}
 							`}
     >
-      {data => data && data.allMarkdownRemark &&  data.allMarkdownRemark.edges ? data.allMarkdownRemark.edges
-        .filter(({ node: post }) => post.frontmatter.releaseType === section.type)
-        .map(({node: post}) => {
-          console.log(post);
-          return (
-            <ReleaseItem
-              key={post.id}
-              slug={post.fields.slug}
-              data={post.frontmatter}
-            />
-          )
-        }
-        )
+      {data => data && data.allMarkdownRemark &&  data.allMarkdownRemark.edges ?           
+        <Trail
+          items={data.allMarkdownRemark.edges.filter(({ node: post }) => post.frontmatter.releaseType === section.type)}
+          keys={item => item.key}
+          from={{ transform: 'translate3d(0,-40px,0)' }}
+          to={{ transform: 'translate3d(0,0px,0)' }}>
+          {({ node: post }) => props =>
+              <ReleaseItem
+                style={props}
+                key={post.id}
+                slug={post.fields.slug}
+                data={post.frontmatter}
+              />
+          }
+        </Trail>
+        
         :
         <p>Hmmm. there's nothing here yet.</p>
       }
