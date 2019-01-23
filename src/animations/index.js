01781d97f203bdf49b88147edc41fcfcad49813e
Ren Rizzolo <renrizmusic@gmail.com>
2019-01-23 14:27:58 +1100

    add some more animations

--- src/animations/index.js
+++ src/animations/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Transition, Spring, config } from 'react-spring'
+import { Transition, Spring, Trail, config } from 'react-spring'
 
 
 export const FadeZoom = ({items, children, ...rest}) => {
@@ -23,10 +23,48 @@ export const FadeZoom = ({items, children, ...rest}) => {
   );
 }
 
-export const Zoom = ({ children, style, show,...rest }) => (
+export const TrailIn = ({items, component: Component}) => (
+  <Trail
+    items={items} keys={item => item.key}
+    from={{ transform: 'translate3d(0,-40px,0)' }}
+    to={{ transform: 'translate3d(0,0px,0)' }}>
+    {({node: post}) => props =>
+      <div style={props}>
+      <Component 
+        key={post.id}
+        slug={post.fields.slug}
+        data={post.frontmatter} 
+      />
+      </div>
+    }
+  </Trail>
+)
+
+export const InFromTop = ({ children, style, className, show, ...rest }) => (
+  <Spring
+    from={{ transform: show ? 'translateY(-150px)' : 'translateY(0)' }}
+    to={{ transform: show ? 'translateY(0)' : 'translateY(-150px)' }}
+    config={{ tension: 125, friction: 17, delay: 250 }}
+  >
+    {props => <div {...rest} className={className} style={{ ...props, ...style }}>{children}</div>}
+  </Spring>
+)
+
+export const InFromBottom = ({ children, style, className, show, ...rest }) => (
+  <Spring
+    from={{ transform: show ? 'translateY(50px)' : 'translateY(0)' }}
+    to={{ transform: show ? 'translateY(0)' : 'translateY(50px)' }}
+    config={{ tension: 105, friction: 12, delay: 250 }}
+  >
+    {props => <div {...rest} className={className} style={{ ...props, ...style }}>{children}</div>}
+  </Spring>
+)
+
+
+export const Zoom = ({ children, style, show, zoom, ...rest }) => (
   <Spring
     from={{ transform: show ? 'scale(6)' : 'scale(1)' }}
-    to={{ transform: show ? 'scale(1)' : 'scale(1)' }}
+    to={{ transform: zoom ? 'scale(6)' : 'scale(1)' }}
     config={{ tension: 125, friction: 17, delay: 250  }}
   >
     {props => <div {...rest} style={{...props, ...style}}>{children}</div>}
