5fab78d0c117ead0976be1c18920167b640aa2ad
Ren Rizzolo <renrizmusic@gmail.com>
2019-01-23 14:28:16 +1100

    add preview that doesn't work yet

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,7 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ReleasePostPreview from './preview-templates/ReleasePostPreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('release', ReleasePostPreview)
