e452039204a35e526b33f0f792838eb4ee374205
renrizzolo <renrizmusic@gmail.com>
2019-01-26 00:00:24 +1100

    add importable icons

---
+++ src/img/icons/index.js
@@ -0,0 +1,7 @@
+export { default as PrevIcon } from './baseline-fast_rewind-24px.svg'
+export { default as NextIcon } from './baseline-fast_forward-24px.svg'
+export { default as StopIcon } from './baseline-stop-24px.svg'
+export { default as PlayIcon } from './baseline-play_circle_outline-24px.svg'
+export { default as PauseIcon } from './baseline-pause_circle_outline-24px.svg'
+export { default as CloseIcon } from './baseline-close-24px.svg'
+export { default as AlbumIcon } from './baseline-album-24px.svg'
