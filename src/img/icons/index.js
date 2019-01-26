730d96d87bd3e90bec7278e95331f8f9556e49c3
renrizzolo <renrizmusic@gmail.com>
2019-01-26 12:09:49 +1100

    make unified icon component with API like react-native-vector-icons

--- src/img/icons/index.js
+++ src/img/icons/index.js
@@ -1,7 +1,33 @@
-export { default as PrevIcon } from './baseline-fast_rewind-24px.svg'
-export { default as NextIcon } from './baseline-fast_forward-24px.svg'
-export { default as StopIcon } from './baseline-stop-24px.svg'
-export { default as PlayIcon } from './baseline-play_circle_outline-24px.svg'
-export { default as PauseIcon } from './baseline-pause_circle_outline-24px.svg'
-export { default as CloseIcon } from './baseline-close-24px.svg'
-export { default as AlbumIcon } from './baseline-album-24px.svg'
+import React from 'react'
+import { default as Prev } from './baseline-fast_rewind-24px.svg'
+import { default as Next } from './baseline-fast_forward-24px.svg'
+import { default as Stop } from './baseline-stop-24px.svg'
+import { default as Play } from './baseline-play_circle_outline-24px.svg'
+import { default as Pause } from './baseline-pause_circle_outline-24px.svg'
+import { default as Close } from './baseline-close-24px.svg'
+import { default as Album } from './baseline-album-24px.svg'
+import { default as Music } from './baseline-queue_music-24px.svg'
+
+const components = {
+  play: Play,
+  pause: Pause,
+  prev: Prev,
+  next: Next,
+  stop: Stop,
+  close: Close,
+  album: Album,
+  music: Music,
+};
+
+export const Icon = ({ name, style, className, size, children }) => {  
+  const IconComponent = components[name]
+  return ( 
+    <IconComponent width={size} height={size} style={style} className={className}>
+      {children}
+    </IconComponent>
+  )
+}
+
+Icon.defaultProps = {
+  size: 24,
+}
