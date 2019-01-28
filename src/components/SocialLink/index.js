60bef0240b6787b19b20f73f727746ea80b2cd14
renrizzolo <renrizmusic@gmail.com>
2019-01-28 16:12:19 +1100

    use Icon component

--- src/components/SocialLink/index.js
+++ src/components/SocialLink/index.js
@@ -1,58 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Icon } from '../../img/icons'
 
-const getIcon = icon => {
-  switch (icon) {
-    case 'facebook':
-      return Fb
-    case 'twitter':
-      return Tw
-    case 'soundcloud':
-      return Sc
-    default:
-      return null
-      break
-  }
-}
-const SocialLink = props => {
-  const icon = getIcon(props.icon)
-  return props.url ? (
-    <a className="social-link" href={props.url} title={props.title}>
-      {icon}
-      {props.title}
+const SocialLink = ({url, title, icon}) => {
+  return url ? (
+    <a className="social-link" href={url} title={title}>
+      {icon && <Icon name={icon}/>}
+      {title}
     </a>
   ) : (
-    <p>{props.title}</p>
+    <p>{title}</p>
   )
 }
 
-const Fb = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-  >
-    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
-  </svg>
-)
-const Sc = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-  >
-    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-6.417 14.583c-.354-.318-.583-.79-.583-1.323 0-.532.229-1.003.583-1.323v2.646zm1.167.417c-.212 0-.323.003-.583-.08v-3.318c.276-.088.407-.085.583-.071v3.469zm1.167 0h-.584v-3.305l.18.105c.08-.328.222-.628.404-.895v4.095zm1.166 0h-.583v-4.706c.18-.134.373-.25.583-.33v5.036zm1.167 0h-.583v-5.167c.22-.023.286-.04.583.005v5.162zm1.167 0h-.584v-4.987l.222.107c.104-.181.228-.346.362-.5v5.38zm5.885 0h-5.302v-5.904c.465-.32 1.016-.512 1.611-.512 1.583 0 2.866 1.307 2.984 2.962 1.14-.558 2.405.34 2.405 1.642 0 1-.761 1.812-1.698 1.812z" />
-  </svg>
-)
-const Tw = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-  >
-    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6.066 9.645c.183 4.04-2.83 8.544-8.164 8.544-1.622 0-3.131-.476-4.402-1.291 1.524.18 3.045-.244 4.252-1.189-1.256-.023-2.317-.854-2.684-1.995.451.086.895.061 1.298-.049-1.381-.278-2.335-1.522-2.304-2.853.388.215.83.344 1.301.359-1.279-.855-1.641-2.544-.889-3.835 1.416 1.738 3.533 2.881 5.92 3.001-.419-1.796.944-3.527 2.799-3.527.825 0 1.572.349 2.096.907.654-.128 1.27-.368 1.824-.697-.215.671-.67 1.233-1.263 1.589.581-.07 1.135-.224 1.649-.453-.384.578-.87 1.084-1.433 1.489z" />
-  </svg>
-)
+SocialLink.propTypes = {
+  url: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+}
+
 export default SocialLink
