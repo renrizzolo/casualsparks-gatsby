126f951559a62a22241797bd4867e87bcff79459
renrizzolo <renrizmusic@gmail.com>
2019-01-28 14:56:17 +1100

    add regular play icon

--- src/img/icons/index.js
+++ src/img/icons/index.js
@@ -3,6 +3,8 @@ import { default as Prev } from './baseline-fast_rewind-24px.svg'
 import { default as Next } from './baseline-fast_forward-24px.svg'
 import { default as Stop } from './baseline-stop-24px.svg'
 import { default as Play } from './baseline-play_circle_outline-24px.svg'
+import { default as PlayArrow } from './baseline-play_arrow-24px.svg'
+
 import { default as Pause } from './baseline-pause_circle_outline-24px.svg'
 import { default as Close } from './baseline-close-24px.svg'
 import { default as Album } from './baseline-album-24px.svg'
@@ -10,6 +12,7 @@ import { default as Music } from './baseline-queue_music-24px.svg'
 
 const components = {
   play: Play,
+  play_arrow: PlayArrow,
   pause: Pause,
   prev: Prev,
   next: Next,
