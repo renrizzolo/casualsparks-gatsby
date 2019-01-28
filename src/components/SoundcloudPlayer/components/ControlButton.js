c879680ffb6a4b6c06a36c95524a47b322bd5add
renrizzolo <renrizmusic@gmail.com>
2019-01-28 14:55:06 +1100

    add disabled

--- src/components/SoundcloudPlayer/components/ControlButton.js
+++ src/components/SoundcloudPlayer/components/ControlButton.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Icon } from '../../../img/icons'
 
-const ControlButton = ({ className, fn, icon, size }) => (
-  <a onClick={fn} className={`sc-player__button ${className}`}>
+const ControlButton = ({disabled, className, fn, icon, size }) => (
+  <a onClick={!disabled ? fn : undefined} className={`sc-player__button ${className}`}>
     <Icon name={icon} size={size} className={`sc-player__icon ${className}`} />
   </a>
 )
