b81fc9748534b8e3178768ba49519ad4bdb90709
renrizzolo <renrizmusic@gmail.com>
2019-01-28 16:12:00 +1100

    add markdown converter

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import showdown from 'showdown'
+
+const converter = new showdown.Converter()
+
+export const MarkdownContent = ({ content, className }) => (
+  <div className={className} dangerouslySetInnerHTML={{ __html: converter.makeHtml(content) }} />
+)
+
+MarkdownContent.propTypes = {
+  content: PropTypes.string,
+  className: PropTypes.string,
+}
+
 
 export const HTMLContent = ({ content, className }) => (
   <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
