727539b5a670c5d2c6238d0c1123deabb9db65d3
Ren Rizzolo <renrizmusic@gmail.com>
2019-04-02 13:15:34 +1100

    add gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,8 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+
     {
       resolve: `gatsby-plugin-layout`,
       options: {
