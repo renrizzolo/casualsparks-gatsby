e20296976da6276c69cec39089c0ff5070c0412f
renrizzolo <renrizmusic@gmail.com>
2019-07-07 20:27:28 +1000

    fix title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
-import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
-import CircleLayout from './CircleLayout';
-import { Fade } from '../animations';
-import Navbar from './Navbar';
-import '../styles/index.scss';
-import SoundcloudPlayerProvider, { SoundcloudPlayerUI } from './SoundcloudPlayer';
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import { StaticQuery, graphql } from "gatsby";
+import CircleLayout from "./CircleLayout";
+import { Fade } from "../animations";
+import Navbar from "./Navbar";
+import "../styles/index.scss";
+import SoundcloudPlayerProvider, {
+  SoundcloudPlayerUI
+} from "./SoundcloudPlayer";
 
 class TemplateWrapper extends Component {
   state = {
     mounted: false,
-    zoom: false,
+    zoom: false
   };
 
   componentDidMount = () => {
@@ -21,16 +23,18 @@ class TemplateWrapper extends Component {
 
     setTimeout(() => {
       this.setState({
-        mounted: true,
+        mounted: true
       });
     }, 1000);
   };
   setScrollBarWidth = () => {
-    const scrollEl = document.getElementById('scroll-width');
+    const scrollEl = document.getElementById("scroll-width");
     const scrollbarWidth = scrollEl.offsetWidth - scrollEl.clientWidth;
     const root = document.documentElement;
-    const originalOffset = getComputedStyle(root).getPropertyValue('--menu-offset');
-    root.style.setProperty('--menu-offset', originalOffset - scrollbarWidth);
+    const originalOffset = getComputedStyle(root).getPropertyValue(
+      "--menu-offset"
+    );
+    root.style.setProperty("--menu-offset", originalOffset - scrollbarWidth);
   };
 
   // componentWillReceiveProps(nextProps) {
@@ -49,7 +53,7 @@ class TemplateWrapper extends Component {
 
   render() {
     const { children, pageContext } = this.props;
-    console.log('context', pageContext);
+    console.log("context", pageContext);
     return (
       <StaticQuery
         query={graphql`
@@ -62,12 +66,17 @@ class TemplateWrapper extends Component {
             }
           }
         `}
-        render={(data) => (
+        render={data => (
           <div>
             <Helmet>
               <html lang="en" />
-              <title>{data.site.siteMetadata.title}</title>
-              <meta name="description" content={data.site.siteMetadata.description} />
+              <title>{`${data.site.siteMetadata.title} - ${
+                pageContext.title
+              }`}</title>
+              <meta
+                name="description"
+                content={data.site.siteMetadata.description}
+              />
               <link
                 rel="icon"
                 type="image/png"
@@ -76,9 +85,16 @@ class TemplateWrapper extends Component {
               />
               <meta name="theme-color" content="#fff" />
               <meta property="og:type" content="business.business" />
-              <meta property="og:title" content={data.site.siteMetadata.title} />
+              <meta
+                property="og:title"
+                content={`${data.site.siteMetadata.title}${pageContext.title &&
+                  "-" + pageContext.title}`}
+              />
               <meta property="og:url" content="/" />
-              <meta property="og:image" content="/img/Casual-Sparks-light-blue-600.png" />
+              <meta
+                property="og:image"
+                content="/img/Casual-Sparks-light-blue-600.png"
+              />
             </Helmet>
             <div className="scroll-width" id="scroll-width" />
             <Navbar />
@@ -90,12 +106,15 @@ class TemplateWrapper extends Component {
               <SoundcloudPlayerProvider clientId="a7c99e975fa37c393cb1a6d89d5c1e0b">
                 {pageContext && pageContext.layout && (
                   <div>
-                    {pageContext && pageContext.layout === 'square' ? (
+                    {pageContext && pageContext.layout === "square" ? (
                       <Fade show={this.state.mounted}>
-                        {(style) => <div style={style}>{children}</div>}
+                        {style => <div style={style}>{children}</div>}
                       </Fade>
                     ) : (
-                      <CircleLayout show={!this.state.mounted} zoom={this.state.zoom}>
+                      <CircleLayout
+                        show={!this.state.mounted}
+                        zoom={this.state.zoom}
+                      >
                         {children}
                       </CircleLayout>
                     )}
