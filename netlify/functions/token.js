31c14f7010f1fd5f335e22f6cee66e10355e49e8
renrizzolo <renrizmusic@gmail.com>
2022-05-27 00:57:25 +1000

    fix: token

--- netlify/functions/token.js
+++ netlify/functions/token.js
@@ -1,19 +1,20 @@
 const API_ENDPOINT = "https://api.soundcloud.com/oauth2/token";
 const fetch = require("isomorphic-fetch");
-let cachedData;
 
+let cachedData;
 
 // gets an access_token for OAUTH requests
 exports.handler = async (event, context) => {
   try {
-    const { access_token , expires_at } = cachedData
+
+    const { access_token, expires_at } = cachedData || {}
     if (access_token && (expires_at * 1000) < Date.now()) {
       return {
         statusCode: 200,
         body: JSON.stringify({ data: cachedData, cached: true }),
       };
     }
-    
+
     console.log({
       grant_type: "client_credentials",
       client_id: process.env.SOUNDCLOUD_CLIENT_ID,
@@ -45,7 +46,7 @@ exports.handler = async (event, context) => {
 
     console.log("returning", data);
     cachedData = data;
-    
+
     return {
       statusCode: 200,
       headers: {
